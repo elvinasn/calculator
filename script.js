a8115f1df0a3c70e81e5eee1e7ccba187a06ab12
Elvinas <elviz.nejuz@gmail.com>
2022-03-15 22:07:15 +0200

    added design

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+function operate(a, b, operator){
+    if(operator === '+'){
+        return add(a, b);
+    }
+    else if(operator === '-'){
+        return subtract(a, b)
+    }
+    else if(operator === '*'){
+        return multiply(a, b)
+    }
+    else if(operator === '/'){
+        return divide(a, b);
+    }
+}
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    if(b == 0){
+        return "ERROR";
+    }
+    return a / b;
+}
